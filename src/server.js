a66b4816854a700cd746ffa2c1ce0a0d096169bf
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-24 23:22:12 +1000

    adding arfticles copmments

--- src/server.js
+++ src/server.js
@@ -1,35 +1,53 @@
-import express from 'express';
+import express from "express";
 
-let articlesInfo = [{
-    name: 'learn-react',
+let articlesInfo = [
+  {
+    name: "learn-react",
     upvotes: 0,
-},
-{
-    name: 'learn-node',
+    comments: [],
+  },
+  {
+    name: "learn-node",
     upvotes: 0,
-},
-{
-    name: 'mongodb',
+    comments: [],
+  },
+  {
+    name: "mongodb",
     upvotes: 0,
-}
-]
+    comments: [],
+  },
+];
 
 const app = express();
 app.use(express.json());
 
-app. put('/api/articles/:name/upvote', (req, res) => {
-    const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
-    if (article) {
-        article.upvotes += 1;
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
-    } else {
-        res.send('That article doesn\'t exist');
-    }
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send("That article doesn't exist");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("That article doesn't exist");
+  }
 });
 
 app.listen(8000, () => {
-    console.log('Server is listing');
+  console.log("Server is listing");
 });
 
 // app.post('/hello', (req, res) => {
