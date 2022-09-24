9e1d79d69926366ac580d708683ff7e30add549d
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-24 23:00:35 +1000

    first commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+},
+{
+    name: 'learn-node',
+    upvotes: 0,
+},
+{
+    name: 'mongodb',
+    upvotes: 0,
+}
+]
+
+const app = express();
+app.use(express.json());
+
+app. put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Server is listing');
+});
+
+// app.post('/hello', (req, res) => {
+//     res.send(`${req.body.name}`);
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     const  {name} = req.params;
+//     res.send(`${name}!`);
+// })
